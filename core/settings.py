4b87f8e2a7e4de79a0b5d45d47daf6af12f6b153
Tiago Hinterholz <fh.tiago@gmail.com>
2025-10-15 22:00:51 -0300

    Fix fallback to SQLite3

--- core/settings.py
+++ core/settings.py
@@ -127,12 +127,12 @@ WSGI_APPLICATION = "core.wsgi.application"
 # ===================== BANCO DE DADOS =====================
 DATABASES = {
     "default": {
-        "ENGINE": os.getenv("ENGINE_DB"),
-        "NAME": os.getenv("NAME_DB"),
-        "USER": os.getenv("USER_DB"),
-        "PASSWORD": os.getenv("PASSWORD_DB"),
-        "HOST": os.getenv("HOST_DB"),
-        "PORT": os.getenv("PORT_DB"),
+        "ENGINE": os.getenv("ENGINE_DB", "django.db.backends.sqlite3"),
+        "NAME": os.getenv("NAME_DB", os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER": os.getenv("USER_DB", ""),
+        "PASSWORD": os.getenv("PASSWORD_DB", ""),
+        "HOST": os.getenv("HOST_DB", ""),
+        "PORT": os.getenv("PORT_DB", ""),
     }
 }
 
